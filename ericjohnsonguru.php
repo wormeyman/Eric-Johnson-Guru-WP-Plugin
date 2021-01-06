8133f246565a7c06eb8fe671f3f69ce5cd7b90f0
Eric J <wormeyman@gmail.com>
2021-01-05 21:42:26 -0800

    Create ericjohnsonguru.php

---
+++ ericjohnsonguru.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Eric Johnson Guru Plugin
+Plugin URI: https://ericjohnson.guru/
+Description: Code Snippets for my lovely Clients
+Version: 1.0.0
+Author: Eric Johnson
+Author URI: https://ericjohnson.guru/
+License: MIT
+License URI: https://opensource.org/licenses/MIT
+*/
+
+// https://docs.wp-rocket.me/article/7-enabling-white-label
+// White Label WP ROCKET.
+define ('WP_ROCKET_WHITE_LABEL_ACCOUNT', true);
